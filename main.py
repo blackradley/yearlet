05ef31193c001010f391c4ed6b963b08a3da6a85
joejcollins <joejcollins@hotmail.com>
2018-04-13 16:41:56 +0100

    Running in Visual Studio 2017

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 from package.twitter_sentiments import TwitterSentiments
 
 SENTIMENTS = TwitterSentiments()
+
 TWEETS = SENTIMENTS.search("Tendring")
 for tweet in TWEETS:
     print u"{0}".format(tweet.text).encode('utf-8', 'ignore')
