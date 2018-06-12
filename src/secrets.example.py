a293823289e257c61cebfebd0ffd301b50bf2df9
Joe Colllins <joejcollins@gmail.com>
2018-06-12 14:59:39 +0100

    Maybe start now.

--- src/secrets.example.py
+++
@@ -1,7 +0,0 @@
-''' Secrets not commited to version control '''
-class Secrets(object):# pylint: disable=too-few-public-methods
-    ''' Secrets from https://apps.twitter.com/ '''
-    CONSUMER_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxx"
-    CONSUMER_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-    ACCESS_TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
-    ACCESS_TOKEN_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
